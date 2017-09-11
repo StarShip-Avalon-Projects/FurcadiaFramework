20cad2dcb7ed47d657ce2deb7ee737834a6eaaf7
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-11 01:49:26 -0700

    Auto Version Update

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -12,5 +12,6 @@ msbuild /t:BuildAll  Solution.build
 
 :End
 git add --all
-git commit -m"Update docs" --all
+git commit -m"Auto Version Update" --all
 git push
+git request-pull master https://github.com/StarShip-Avalon-Projects/FurcadiaFramework.git
