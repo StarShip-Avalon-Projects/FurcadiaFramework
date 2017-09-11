82c6def0e8d92d34a064879d9a3d66a82cf09d72
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-11 00:36:09 -0700

    essageupdate docs

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -1,2 +1,13 @@
+
+git pull --all
+IF "%~1"=="" GOTO BuildAll
+IF "%~1"=="VersionBump" GOTO VersionBump
+
+:VersionBump
+msbuild /t:IncrementVersions;BuildAll  Solution.build
+goto :End
+
+:BuildAll
 msbuild /t:BuildAll  Solution.build
-pause
+
+:End
