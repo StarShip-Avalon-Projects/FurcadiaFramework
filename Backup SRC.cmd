482d80b08549fca19aeaf757d43fd30a593fe81d
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-29 12:07:24 -0700

    Auto Version Update and Update Docs

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -6,13 +6,27 @@ IF "%~1"=="VersionBump" GOTO VersionBump
 
 :VersionBump
 msbuild /t:IncrementVersions;BuildAll  Solution.build
-goto :End
-
+set BUILD_STATUS=%ERRORLEVEL% 
+if %BUILD_STATUS%==0 goto end 
+if not %BUILD_STATUS%==0 goto fail 
+ 
 :BuildAll
 msbuild /t:BuildAll  Solution.build
+set BUILD_STATUS=%ERRORLEVEL% 
+if %BUILD_STATUS%==0 goto end 
+if not %BUILD_STATUS%==0 goto fail 
+ 
+:fail 
+pause 
+exit /b 1
 
 :End
 git add --all
-git commit -m"Auto Version Update" --all
-git push
+git commit -m"Auto Version Update and Update Docs" --all
+
+git submodule foreach "git add --all"
+git submodule foreach "git commit -m'Auto Update SubModules'-a"
+git submodule foreach "git push -f origin HEAD:master"
+git push -f --all --recurse-submodules=on-demand
+
 git request-pull master https://github.com/StarShip-Avalon-Projects/FurcadiaFramework.git
