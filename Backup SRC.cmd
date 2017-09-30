6877856c7cf3f6ecbd1ceb00070a572498b32358
Jeremy Trombley <gerolkae@hotmail.com>
2017-09-29 19:43:28 -0700

    pft

--- Backup SRC.cmd
+++ Backup SRC.cmd
@@ -1,4 +1,6 @@
 call UpdateSrc.cmd
+set BUILD_STATUS=%ERRORLEVEL% 
+if not %BUILD_STATUS%==0 goto fail 
 
 IF "%~1"=="" GOTO BuildAll
 IF "%~1"=="VersionBump" GOTO VersionBump
@@ -28,8 +30,6 @@ exit /b 1
 :End
 
 git add --all
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto eof 
 
 git submodule foreach "git add --all"
 set GIT_STATUS=%ERRORLEVEL% 
@@ -37,14 +37,19 @@ if not %GIT_STATUS%==0 goto eof
 
 git submodule foreach "git commit -m'Auto Update SubModules'"
 
+git submodule foreach "git push origin HEAD"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto eof
+
 git commit -m"Auto Version Update"
 set GIT_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto eof 
 
 git push --recurse-submodules=on-demand
 set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto eof 
+if not %GIT_STATUS%==0 goto eof
 
+:pull
 git request-pull master https://github.com/StarShip-Avalon-Projects/FurcadiaFramework.git
 
 :eof
