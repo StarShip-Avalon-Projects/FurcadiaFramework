4ed1ab84268e55ddbc84d478ff1a87340dcd22e4
Jeremy Trombley <gerolkae@hotmail.com>
2017-11-17 02:10:18 -0700

    Auto Version Update

--- UpDateSrc.cmd
+++ UpDateSrc.cmd
@@ -1,11 +1,19 @@
 
-git.exe pull --recurse-submodules=yes
+git.exe pull --recurse-submodules=on-demand
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+bin\nuget.exe restore
 set GIT_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto fail 
 
 :eof
+
 exit /b 0
 
+
 :fail 
+
 pause 
-exit /b 1
+
+:exit /b 1
