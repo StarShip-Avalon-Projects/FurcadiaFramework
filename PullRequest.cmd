dd9c562109ceb83d7b5dcfe32be7b4287bd915de
Jeremy Trombley <gerolkae@hotmail.com>
2018-02-04 21:27:50 -0700

    grrr

--- PullRequest.cmd
+++ PullRequest.cmd
@@ -1,2 +1,2 @@
 :pull
-git request-pull v2.19.x.Elta https://github.com/StarShip-Avalon-Projects/Silver-Monkey.git v2.19.x.Elta
+git request-pull master https://github.com/StarShip-Avalon-Projects/FurcadiaFramework.git master
--- UpDateSrc.cmd
+++ UpDateSrc.cmd
@@ -1,14 +1,21 @@
 
-git.exe pull --recurse-submodules=yes
-set GIT_STATUS=%ERRORLEVEL% 
-if not %GIT_STATUS%==0 goto fail 
-
-bin\nuget.exe restore
+git.exe pull --recurse-submodules=on-demand
 set GIT_STATUS=%ERRORLEVEL% 
 if not %GIT_STATUS%==0 goto fail 
 
 :NuGetRestore
-bin\nuget.exe restore
+
+bin\nuget.exe update -self
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+bin\nuget.exe restore FurcadiaFramework.sln
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+bin\nuget.exe update FurcadiaFramework.sln
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
 
 :eof
 exit /b 0
