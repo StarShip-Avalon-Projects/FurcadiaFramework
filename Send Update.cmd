1d2dfe69317c00e53aad406ce56595a96860baaf
Jeremy Trombley <gerolkae@hotmail.com>
2018-02-24 13:48:39 -0700

    Auto Update SubModules

--- Send Update.cmd
+++ Send Update.cmd
@@ -1,21 +1,24 @@
 
 git.exe add --all
+git.exe submodule foreach "git.exe add --all || true"
+rem set git.exe_STATUS=%ERRORLEVEL% 
+rem if not %git.exe_STATUS%==0 goto eof 
 
-git.exe submodule foreach "git.exe add --all"
+git.exe submodule foreach "git.exe commit -am'Auto Update SubModules' || true"
+git commit -am"Auto Update SubModules"
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
+
+git push --recurse-submodules=on-demand
 set git.exe_STATUS=%ERRORLEVEL% 
-if not %git.exe_STATUS%==0 goto eof 
+if %GIT_STATUS%==0 goto eof
 
-git.exe submodule foreach "git.exe commit -m'Auto Update SubModules'"
-git.exe commit -m"Auto Version Update"
-set git.exe_STATUS=%ERRORLEVEL% 
-if not %git.exe_STATUS%==0 goto eof 
-
-git.exe push --recurse-submodules=on-demand
-set git.exe_STATUS=%ERRORLEVEL% 
-if not %git.exe_STATUS%==0 goto eof
-
-:PullRequest
-call PullRequest.cmd
+git push
+set GIT_STATUS=%ERRORLEVEL% 
+if not %GIT_STATUS%==0 goto fail 
 
 :eof
 exit /b 0
+:fail
+pause
+exit /b 1
