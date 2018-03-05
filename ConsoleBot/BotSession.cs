109ef16aa425a41b6b5e036d15a15fd4067df95c
Jeremy Trombley <gerolkae@hotmail.com>
2018-03-05 12:25:20 -0700

    fix stand alone connection and update docs

---
+++ ConsoleBot/BotSession.cs
@@ -0,0 +1,27 @@
+﻿using Furcadia.IO;
+using Furcadia.Net.DirectConnection;
+using Furcadia.Net.Options;
+using Furcadia.Net.Proxy;
+using System;
+using System.Net.NetworkInformation;
+
+namespace BugProxy.Net
+
+{
+    /// <summary>
+    /// </summary>
+    public class BotSession : NetConnection, IDisposable
+    {
+        #region Public Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BotSession"/> class.
+        /// </summary>
+        /// <param name="Options"></param>
+        public BotSession(ClientOptions Options) : base(Options)
+        {
+        }
+
+        #endregion Public Constructors
+    }
+}
